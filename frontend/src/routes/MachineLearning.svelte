24ac91ac07be78c03863a124d0ba3a09ce1c64a7
Stefan Schörkmeier <s.schoerkmeier@gmx.at>
2025-03-26 12:25:19 +0100

    #4 Updated text in machine learning section.

--- frontend/src/routes/MachineLearning.svelte
+++ frontend/src/routes/MachineLearning.svelte
@@ -56,7 +56,16 @@
 
 
 </script>
-<p>TODO write about machine learning</p>
+<p>
+	Machine learning (often referred to as Artificial Intelligence (AI) or Künstliche Intelligenz (KI) in german) has
+	many use cases. Currently, it is widely known for its applications in large language models (LLMs) like ChatGPT.
+	<br><br>
+	This makes sense, because it performs amazing at knowledge work. I personally use it more often than Google,
+	because the results are on average better then reading a random blog post about a topic.
+	<br><br>
+	BUT besides from LLMs we should not forget the other amazing applications of machine learning. One of those is
+	<b>pattern recognition / prediction.</b> Try it out below :)
+</p>
 <div class="canvas-container">
 	<canvas
 		bind:this={canvas}
@@ -118,17 +127,33 @@
 	</div>
 	<div class="prediction-result">
 		<h3>Recognized number: {prediction}</h3>
+		<p>
+			If you are wondering, why it just won't correctly predict your perfect 1 (I did at least)
+			that is because of a fundamental rule of machine learning: It's <b>all about the data</b>. In the
+			<a href="https://en.wikipedia.org/wiki/MNIST_database" target="_blank" rel="noopener noreferrer">
+				MNIST dataset
+			</a>, 1 is often written as a | so our poor model never had a chance to correctly recognize it ...
+		</p>
 	</div>
-	<div>
-		<p>TODO create visualization & talk about MNIST data and why this is interesting to see (for such a "easy" problem
-		we need so many neurons - imaging with real image data! :o cool)</p>
-	</div>
+<!--	<div>-->
+<!--		<p>TODO create visualization & talk about MNIST data and why this is interesting to see (for such a "easy" problem-->
+<!--		we need so many neurons - imaging with real image data! :o cool)</p>-->
+<!--		Probably create a sectoin for each of those-->
+<!--		<h2>TODO doodle to image</h2>-->
+<!--		<h2>TODO LLM to collect feedback</h2>-->
+<!--		<h2>TODO innovation / use prediciton for side navigation</h2>-->
+<!--	</div>-->
+
 </div>
 
 
 
 
 <style>
+
+	p{
+		text-align: left;
+	}
 	.canvas-container {
 		position: relative;
 		width: 500px;
