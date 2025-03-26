163470958da11601e199af8ac24b23790e89936b
Stefan Schörkmeier <s.schoerkmeier@gmx.at>
2025-03-26 18:01:46 +0100

    #4 My first blog post xD

--- frontend/src/routes/blog/data.js
+++ frontend/src/routes/blog/data.js
@@ -2,9 +2,19 @@
 export const posts = [
 	{
 		slug: 'welcome',
-		title: 'placeholder 1',
+		title: 'Code generation and problems I see for the future',
 		content:
-			'<p>foo</p><p>bar</p>'
+			'<p>According to CEOs of top AI companies a large amount of code will be written by AI. ' +
+			'Anthropic CEO Dario Amodei mentioned that 90% of code in 2025 and 100% of code in 2025. ' +
+			'</p>' +
+			'<p>This might be the case, that we are at least working in a pair-progrmaming manner aka ' +
+			'that the code is generated but there is a human in the loop. The <b>problem</b> I have seen from this ' +
+			'project as example is, that LLMs are terrible with new technologies / frameworks. That makes sense ' +
+			'since they simply don\'t have enough training data. </p>' +
+			'<p>With the hype of LLMs replacing or enhancing many engineers, we most likely will mainly use old ' +
+			'framworks where the LLMs can generate good code quality. But this is obviously a huge <b>secuirty issue</b> ' +
+			'right? Imagine that 90% of the code will be with outdated versions with known security issues.</p>' +
+			'<p>So I don\'t advice of not using those tools, but I do believe we have to keep such things in mind.</p>'
 	},
 
 	{
