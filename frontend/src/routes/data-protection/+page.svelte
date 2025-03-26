4d863d84af9be2b5327638567a77a475a7eea402
Stefan Schörkmeier <s.schoerkmeier@gmx.at>
2025-03-26 20:04:37 +0100

    #4 Created privacy policy.

--- frontend/src/routes/data-protection/+page.svelte
+++ frontend/src/routes/data-protection/+page.svelte
@@ -1,1 +1,231 @@
-<h1>foo</h1>
+<script>
+    const EMAIL = "s.schoerkmeier+business@gmail.com"
+</script>
+
+<div class="container">
+    <h3>Deutsche Datenschutzerklärung unten</h3>
+    <h1>Privacy Policy</h1>
+    <h2>Introduction</h2>
+    <p>
+        I value your privacy and I'm committed to protecting your personal data. As suggested by Art 12 and 13 of the
+        General Data Protection Regulation (GDPR) this Privacy Policy explains how I
+        collect, use, and protect your information in compliance with the GDPR (Regulation (EU) 2016/679).
+    </p>
+    <br>
+
+    <h2>What Data is Collected</h2>
+    <p>
+        I collect the following personal data when you voluntarily provided them or they were important to fulfill my
+        obligations:<br><br>
+        &bull; Name / Company<br>
+        &bull; Contact information (Phone, Email address, ...)<br>
+        &bull; Postal Address<br>
+        &bull; Bank Details<br>
+        &bull; VAT-ID<br>
+        &bull; Register number<br>
+        &bull; ...<br>
+    </p>
+
+    <h2>Legal Basis for Processing</h2>
+    <p>
+        The legal basis for processing your personal data is given by
+    </p>
+    <ol>
+        <li>Your <b>explicit consent</b> (Art. 6, 1. (a) GDPR), which you provide when you reached
+            out to me.</li>
+        <li>The data is necessary for the <b>performance of a contract</b> (Art. 6, 1, (b) GDPR) which you are a part
+            of.</li>
+        <li>I must comply with <b>legal obligations</b> (Art. 6, 1, (c) GDPR). For example I must store invoices which
+            might contain your personal data.</li>
+        <li>The data is necessary for the purposes of <b>legitimate interests</b> (Art. 6, 1, (f) GDPR). For example
+            I will store your data if I would like to work with you again.</li>
+    </ol>
+
+    <h2>Withdrawal</h2>
+    <p>
+        You can withdraw you explicit consent at any time. After the <b>withdrawal</b> I will stop processing you data
+        starting the date of your withdrawal. Note that your already provided data will be deleted unless they are
+        required to finish the contract with you or required by law. <b>If you want to withdrawal, please write a mail
+        to: </b> {EMAIL}
+    </p>
+
+    <h2>Data Retention</h2>
+    <p>
+        I will retain your personal data only as long as necessary to respond to your inquiry. Once your inquiry is
+        resolved, your data will be deleted unless required by law to retain it for a longer period or one of
+        the other reasons above.
+    </p>
+
+    <h2>Your Rights</h2>
+    <p>
+        In connection with the processing of your personal data, you have the following rights at any time,
+        which can be exercised by contacting me, Stefan Schörkmeier as the data controller via {EMAIL}
+    </p>
+     <ol>
+        <li><b>Right to access</b> your personal data (Art. 15 GDPR) </li>
+        <li><b>Right for rectification</b> (Article 16 GDPR), <b>erasure</b> ((Article 17 GDPR) and
+            <b>restriction of processing </b> (Article 18 GDPR) </li>
+        <li><b>Right to data portability</b> (Article 20 GDPR) </li>
+        <li><b>Right to object</b> (Article 21 GDPR)</li>
+        <li><b>Right to withdraw consent</b> (Article 7(3) GDPR), whereby the lawfulness of the processing carried out
+            based on the consent until its withdrawal remains unaffected.</li>
+    </ol>
+    <p>
+        Additionally, you have the <b>right to lodge a complaint</b> (Article 77 GDPR), which can be submitted to a
+        supervisory authority. In Austria, this is the <b>Austrian Data Protection Authority</b>: <br><br>
+        Barichgasse 40-42 <br>
+        1030 Vienna <br>
+        Phone: +43 1 52 152-0, <br>
+        Email: dsb@dsb.gv.at <br>
+    </p>
+
+    <h2>Data Security</h2>
+    <p>
+        I implement appropriate technical and organizational measures to protect your personal data from unauthorized
+        access, loss, or misuse. However, no method of transmission over the internet is 100% secure, and I cannot
+        guarantee absolute security.
+    </p>
+
+    <h2>Third-Party Sharing</h2>
+    <p>
+        I do not share your personal data with any third parties unless required by law.
+    </p>
+
+    <h2>Changes to This Privacy Policy</h2>
+    <p>
+        I may update this Privacy Policy from time to time to reflect changes in our practices or legal requirements.
+        The latest version will always be available on this page.
+    </p>
+
+    <h2>Contact Information of Data Controller</h2>
+    <p>
+        This policy has been crafted by Stefan Schörkmeier. If you have any question about this privacy policy,
+        your data, or anything else, feel free to reach out to me. <br><br>
+        Stefan Schörkmeier<br>
+        Scheigergasse 96/8<br>
+        8010 Graz<br>
+        Austria<br><br>
+        E-Mail: {EMAIL}<br>
+        Imprint: <a href="/imprint">schoerkmeier.dev/imprint</a><br>
+    </p>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+    <br>
+    <hr>
+    <br>
+    <br>
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+    <h1>Datenschutzerklärung</h1>
+    <h2>Einleitung</h2>
+    <p>
+        Ich schätze Ihre Privatsphäre und bin verpflichtet, Ihre persönlichen Daten zu schützen. Gemäß Art. 12 und 13 der
+        Datenschutz-Grundverordnung (DSGVO) erklärt diese Datenschutzerklärung, wie ich Ihre Informationen sammle, nutze
+        und schütze. Dies geschieht in Übereinstimmung mit der DSGVO (Verordnung (EU) 2016/679).
+    </p>
+    <br>
+
+    <h2>Welche Daten werden gesammelt</h2>
+    <p>
+        Ich sammle die folgenden personenbezogenen Daten, wenn Sie diese freiwillig bereitstellen oder sie notwendig sind,
+        um meine Verpflichtungen zu erfüllen:<br><br>
+        &bull; Name / Firma<br>
+        &bull; Kontaktdaten (Telefon, E-Mail-Adresse, ...)<br>
+        &bull; Postanschrift<br>
+        &bull; Bankdaten<br>
+        &bull; UID <br>
+        &bull; Firmenbuchnummer<br>
+        &bull; ...<br>
+    </p>
+
+    <h2>Rechtsgrundlage für die Verarbeitung</h2>
+    <p>
+        Die Rechtsgrundlage für die Verarbeitung Ihrer personenbezogenen Daten ergibt sich aus:
+    </p>
+    <ol>
+        <li>Ihrer <b>ausdrücklichen Einwilligung</b> (Art. 6 Abs. 1 lit. a DSGVO), die Sie erteilen, wenn Sie mich
+            kontaktieren.</li>
+        <li>Der Notwendigkeit der Daten für die <b>Erfüllung eines Vertrags</b> (Art. 6 Abs. 1 lit. b DSGVO), dessen
+            Vertragspartei Sie sind.</li>
+        <li>Der Erfüllung von <b>rechtlichen Verpflichtungen</b> (Art. 6 Abs. 1 lit. c DSGVO). Zum Beispiel bin ich
+            verpflichtet, Rechnungen aufzubewahren, die Ihre personenbezogenen Daten enthalten können.</li>
+        <li>Der Notwendigkeit der Daten für <b>berechtigte Interessen</b> (Art. 6 Abs. 1 lit. f DSGVO). Zum Beispiel
+            speichere ich Ihre Daten, wenn ich in Zukunft wieder mit Ihnen zusammenarbeiten möchte.</li>
+    </ol>
+
+    <h2>Widerruf</h2>
+    <p>
+        Sie können Ihre ausdrückliche Einwilligung jederzeit widerrufen. Nach dem <b>Widerruf</b> werde ich die
+        Verarbeitung Ihrer Daten ab dem Datum des Widerrufs einstellen. Beachten Sie, dass bereits bereitgestellte Daten
+        gelöscht werden, es sei denn, sie sind erforderlich, um den Vertrag mit Ihnen zu erfüllen oder gesetzlich
+        vorgeschrieben. <b>Wenn Sie widerrufen möchten, schreiben Sie bitte eine E-Mail an:</b> {EMAIL}
+    </p>
+
+    <h2>Datenspeicherung</h2>
+    <p>
+        Ich speichere Ihre personenbezogenen Daten nur so lange, wie es notwendig ist, um Ihre Anfrage zu beantworten.
+        Sobald Ihre Anfrage bearbeitet ist, werden Ihre Daten gelöscht, es sei denn, sie müssen aus rechtlichen Gründen
+        länger aufbewahrt werden oder aus einem der oben genannten Gründe.
+    </p>
+
+    <h2>Ihre Rechte</h2>
+    <p>
+        Im Zusammenhang mit der Verarbeitung Ihrer personenbezogenen Daten haben Sie jederzeit die folgenden Rechte,
+        die Sie ausüben können, indem Sie mich, Stefan Schörkmeier, als Verantwortlichen unter {EMAIL} kontaktieren.
+    </p>
+    <ol>
+        <li><b>Recht auf Auskunft</b> über Ihre personenbezogenen Daten (Art. 15 DSGVO)</li>
+        <li><b>Recht auf Berichtigung</b> (Art. 16 DSGVO), <b>Löschung</b> (Art. 17 DSGVO) und
+            <b>Einschränkung der Verarbeitung</b> (Art. 18 DSGVO)</li>
+        <li><b>Recht auf Datenübertragbarkeit</b> (Art. 20 DSGVO)</li>
+        <li><b>Recht auf Widerspruch</b> (Art. 21 DSGVO)</li>
+        <li><b>Recht auf Widerruf der Einwilligung</b> (Art. 7 Abs. 3 DSGVO), wobei die Rechtmäßigkeit der bis zum
+            Widerruf erfolgten Verarbeitung unberührt bleibt.</li>
+    </ol>
+    <p>
+        Zusätzlich haben Sie das <b>Recht auf Beschwerde</b> (Art. 77 DSGVO), die bei einer Aufsichtsbehörde eingereicht
+        werden kann. In Österreich ist dies die <b>österreichische Datenschutzbehörde</b>: <br><br>
+        Barichgasse 40-42 <br>
+        1030 Wien <br>
+        Telefon: +43 1 52 152-0, <br>
+        E-Mail: dsb@dsb.gv.at <br>
+    </p>
+
+    <h2>Datensicherheit</h2>
+    <p>
+        Ich setze geeignete technische und organisatorische Maßnahmen ein, um Ihre personenbezogenen Daten vor
+        unbefugtem Zugriff, Verlust oder Missbrauch zu schützen. Allerdings ist keine Methode der Übertragung über das
+        Internet zu 100 % sicher, und ich kann keine absolute Sicherheit garantieren.
+    </p>
+
+    <h2>Weitergabe an Dritte</h2>
+    <p>
+        Ich gebe Ihre personenbezogenen Daten nicht an Dritte weiter, es sei denn, dies ist gesetzlich vorgeschrieben.
+    </p>
+
+    <h2>Änderungen dieser Datenschutzerklärung</h2>
+    <p>
+        Ich kann diese Datenschutzerklärung von Zeit zu Zeit aktualisieren, um Änderungen in meinen Praktiken oder
+        rechtlichen Anforderungen widerzuspiegeln. Die neueste Version wird immer auf dieser Seite verfügbar sein.
+    </p>
+
+    <h2>Kontaktinformationen des Verantwortlichen</h2>
+    <p>
+        Diese Datenschutzerklärung wurde von Stefan Schörkmeier erstellt. Wenn Sie Fragen zu dieser Datenschutzerklärung,
+        Ihren Daten oder anderen Themen haben, können Sie mich gerne kontaktieren. <br><br>
+        Stefan Schörkmeier<br>
+        Scheigergasse 96/8<br>
+        8010 Graz<br>
+        Österreich<br><br>
+        E-Mail: {EMAIL}<br>
+        Impressum: <a href="/imprint">schoerkmeier.dev/imprint</a><br>
+    </p>
+</div>
+
+
+<style>
+    .container {
+        padding: 1rem 10rem 5rem 5rem
+    }
+
+</style>
