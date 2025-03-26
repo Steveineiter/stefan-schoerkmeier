accc7cac20c3b6efcfde27882aa34b7d1a6445f6
Stefan Schörkmeier <s.schoerkmeier@gmx.at>
2025-03-26 16:40:34 +0100

    #4 Created imprint page.

--- frontend/src/routes/imprint/+page.svelte
+++ frontend/src/routes/imprint/+page.svelte
@@ -1,1 +1,94 @@
-<h1>foo</h1>
+<script>
+    const IBAN = "DE22 1001 1001 2625 9763 37"
+    const BIC = "NTSBDEB1XXX"
+
+</script>
+
+<div class="container">
+    <h3>Deutsche Version unten</h3>
+    <h1>Imprint</h1>
+    <p>Responsible for the content: Stefan Schörkmeier</p>
+    <br>
+
+    <h2>Contact</h2>
+    <p>
+        Web: <a
+                href="https://schoerkmeier.dev" target="_blank" rel="noopener noreferrer" >
+                https://schoerkmeier.dev
+            </a><br>
+        Email: S.Schoerkmeier+business@gmail.com
+    </p>
+    <br>
+
+    <h2>Bank Details</h2>
+    <p>
+        IBAN: {IBAN}<br>
+        BIC: {BIC}
+    </p>
+    <br>
+
+    <h2>Postal Address</h2>
+    <p>
+        Stefan Schörkmeier<br>
+        Scheigergasse 96/8<br>
+        8010 Graz<br>
+    </p>
+    <br>
+
+    <h2>Other</h2>
+    <p>
+        VAT-ID: ATU73070936<br>
+        WKO: Wirtschaftskammer Steiermark
+    </p>
+    <br>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+    <br>
+    <hr>
+    <br>
+    <br>
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+    <h1>Impressum</h1>
+     <p>Für den Inhalt Verantwortlich: Stefan Schörkmeier</p>
+    <br>
+
+    <h2>Kontakt</h2>
+    <p>
+        Web: <a
+                href="https://schoerkmeier.dev" target="_blank" rel="noopener noreferrer" >
+                https://schoerkmeier.dev
+            </a><br>
+        E-Mail: S.Schoerkmeier+business@gmail.com
+    </p>
+    <br>
+
+    <h2>Bankverbindung</h2>
+    <p>
+        IBAN: {IBAN}<br>
+        BIC: {BIC}
+    </p>
+    <br>
+
+    <h2>Postadresse</h2>
+    <p>
+        Stefan Schörkmeier<br>
+        Scheigergasse 96/8<br>
+        8010 Graz<br>
+    </p>
+    <br>
+
+    <h2>Sonstiges</h2>
+    <p>
+        UID: ATU73070936<br>
+        WKO: Wirtschaftskammer Steiermark
+    </p>
+    <br>
+</div>
+
+<style>
+    .container {
+        padding: 1rem 10rem 5rem 5rem
+    }
+
+</style>
