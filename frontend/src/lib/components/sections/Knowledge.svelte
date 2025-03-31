6f76db588e60995e894b296359416c03b7131f30
Stefan Schörkmeier <s.schoerkmeier@gmx.at>
2025-03-31 20:03:45 +0200

    #10 Created knowledge component.

--- frontend/src/lib/components/sections/Knowledge.svelte
+++ frontend/src/lib/components/sections/Knowledge.svelte
@@ -2,10 +2,71 @@
   let { title } = $props();
 </script>
 
-<div class="about-container">
+<div class="knowledge-container">
   <div class="text-content">
     <h2>{title}</h2>
-    <p>Health / Lifestyle and Interesting Topics, books, youtube, ... ffooooooo oooooooooooo ooooooooooo oooooooooooooo</p>
+    <h3>Health & Lifestyle</h3>
+    <p>Health is the most important thing we can have. I think most people would agree. Unfortunately there is so much
+    misinformation out there and many people think diet, for example, is really confusing - me included (I tried many
+      sorts of diet - keto, carnivore, mediterranean, vegan, ...) . This is interesting,
+    because we already know the rough steps to take since the 1980's. Obviously, if you have allergies or an illness these
+    findings might not apply to you - but for 90% or so of the population they do. According to
+      <a href="https://www.youtube.com/watch?v=h2q5qC7NLMY" target="_blank">Dr. Gardner</a> those 7 things are:
+    </p>
+    <ol>
+      <li>Eat a variety of Foods </li>
+      <li>Maintain Ideal Weight </li>
+      <li>Avoid too much Fat, Saturated Fat and Cholesterol </li>
+      <li>Eat foods with Adequate Starch and Fiber </li>
+      <li>Avoid too much Sugar </li>
+      <li>Avoid too much Salt </li>
+      <li>If you drink alcohol, do so in moderation </li>
+    </ol>
+    <p>From <a href="https://www.youtube.com/watch?v=5s2U4GGBZak" target="_blank">newer research</a> we know that alcohol is even bad
+    in low doses. The cool part about those 7 things is, that we can <b>easily know whether something is healthy</b> or not.
+    Eating a Wiener Schnitzel with fries, a dessert, and a beer each day? Unhealthy since we are breaking each of the findings above
+    (except they are really small portions and you eat great for the rest of the day).
+    For me, the best diet to archive that is a <b>plant based whole food</b> diet with 1-2 portions of meat/fish per week.
+    Because with that I fulfill those 7 findings by default while still being flexible enough to going out dining with friends.
+    </p>
+    <p> Besides diet, <b>sleep and exercise</b> are the other parts of a healthy life. In a nutshell we want 7-9 hours
+      of high quality sleep, 2-3 hours of weight lifting and 2-3 hours of moderate cardio per week for optimal health.
+      I personally would also add <b>ergonomics</b> (workspace and for sleep) and <b>stress management</b> (meditation, yoga nidra, progressive muscle relaxation)
+    </p>
+    <p><b>Great sources for the above:</b></p>
+    <ul>
+      <li><a href="https://www.youtube.com/@NutritionMadeSimple" target="_blank"> Nutrition Made Simple! </a> with Dr. Gil Carvalho PhD (IMHO best source for diet)</li>
+      <li><a href="https://drstanfield.com/en-eu/pages/roadmap?srsltid=AfmBOoqXKSliqQUXfBNRABkXEisT_HbeKm8AVW0AXXYJN_cw8ldFpDsN" target="_blank"> Dr. Brad Stanfield - Roadmap </a></li>
+      <li><a href="https://www.youtube.com/@DrBradStanfield" target="_blank"> Dr. Brad Stanfield - Youtube </a></li>
+      <li><a href="https://www.youtube.com/@JeffNippard" target="_blank">Jeff Nippard </a></li>
+      <li><a href="https://www.youtube.com/@RenaissancePeriodization" target="_blank">Renaissance Periodization  </a> with Michael Israelte, PhD </li>
+      <li><a href="https://www.youtube.com/@DrMiloWolf" target="_blank">Dr. Milo Wolf, PhD </a></li>
+    </ul>
+    <p>If you wonder, <b>why I only cite science based sources</b> the reason is simple: Time & results. The amazing thing with science
+    is, that it will work for most people. With that I know I will have great progress with a fraction of time investment I would need to make
+    if I follow the advice of a random Influencer (which might even do harm) and try out EVERYTHING whether it works for me or not.</p>
+
+    <h2>Tech channels</h2>
+    <ul>
+      <li><a href="https://www.youtube.com/@aiexplained-official" target="_blank"> AI Explained </a> - IMHO best source for machine learning/AI news</li>
+      <li><a href="https://www.youtube.com/@Fireship" target="_blank"> Fireship</a> - fun source to stay up to date with developer world</li>
+      <li><a href="https://www.youtube.com/@ArjanCodes" target="_blank"> ArjanCodes</a> - great source for python related topics</li>
+      <li><a href="https://www.youtube.com/@TwoMinutePapers" target="_blank"> Two Minute Papers</a> - source for machine learning/AI news</li>
+      <li><a href="https://www.youtube.com/@fknight" target="_blank"> ForrestKnight</a> - all around / often interesting new topics</li>
+      <li><a href="https://www.youtube.com/@ThePrimeagen" target="_blank"> ThePrimeagen</a> - I basically learned vim motions because of him</li>
+    </ul>
+
+    <h2>Books which gave me great lessons</h2>
+    <ul>
+      <li><b>How to Win Friends & Influence People (Dale Carnegie)</b> - amazing book, I think everybody should read this </li>
+      <li><b>Awaken the Giant Within (Anthony Robbins)</b> - If you have read one selfhelpbook you have read them all, but I
+      liked this one. I got my most important mind set through this book: I can always grow to the kind of person I want to be</li>
+      <li><b>The Lean Startup (Eric Ries)</b> - great read, I think this relevant for each company which works in a dynamic environment </li>
+      <li><b>Zwischen Stahl und Schokolade (Prodanovic; Unterhuber)</b> - interesting stories of austrian businesses and the people behind</li>
+      <li><b>Pattern Recognition and Machine Learning (Christopher Bishop)</b> - I read the whole book in my first term of my Master.
+        I understood around 10% of it so maybe it was a waste of time. But it's an amazing source and I reference it to this date</li>
+      <li><b>Deep Learning (Ian Goodfellow)</b> - same as above</li>
+    </ul>
   </div>
 </div>
 
