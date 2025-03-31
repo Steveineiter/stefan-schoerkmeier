e3bf235a9e788aca8813591c53eb4154512c5f0a
Stefan Schörkmeier <s.schoerkmeier@gmx.at>
2025-03-31 18:19:22 +0200

    #10 Created certificates component.

--- frontend/src/lib/components/sections/Certificates.svelte
+++ frontend/src/lib/components/sections/Certificates.svelte
@@ -6,24 +6,59 @@
   <div class="text-content">
     <h2>{title}</h2>
     <h3>Education</h3>
-    <ul class="education-bullet-points">
-      <li>Master of Science, Softwaredevelopement Management (Major: Machine Learning, Minor: Business Law), Graz univisity of technology, Graz, 2022-ongoing</li>
-      <li>Bachelor of Science, Softwaredevelopement Management, Graz Univieristy of technology, Graz, 2018-2022</li>
+    <ul class="bullet-points">
+      <li>Master of Science, Software Engineering and Management (Major: Machine Learning, Minor: Business Law), Graz University of Technology, Graz, 2022-ongoing</li>
+      <li>Bachelor of Science, Software Engineering and Management, Graz University of Technology, Graz, 2018-2022</li>
     </ul>
+    <hr>
 
     <h3>Kaggle (selection)</h3>
-    <ul class="other-bullet-points">
-      <li>Oxford english test</li>
+    <ul class="bullet-points">
+      <li><a href="/certificates/kaggle-python-certificate.png" target="_blank">Python</a></li>
+      <li><a href="/certificates/kaggle-computer-vision-certificate.png" target="_blank">Computer Vision</a></li>
+      <li><a href="/certificates/kaggle-intro-to-deep-learning-certificate.png" target="_blank">Intro to Deep Learning</a></li>
+      <li><a href="/certificates/kaggle-natural-language-processing-certificate.png" target="_blank">Natural Language Processing</a></li>
+      <li><a href="/certificates/kaggle-intermediate-machine-learning-certificate.png" target="_blank">Intermediate Machine Learning</a></li>
     </ul>
+    <hr>
 
     <h3>Udemy</h3>
-    <ul class="other-bullet-points">
-      <li>Oxford english test</li>
+    <p><b>Programming Languages</b></p>
+    <ul class="bullet-points">
+      <li><a href="/certificates/python-certificate.pdf" target="_blank">Python</a></li>
+      <li><a href="/certificates/c-certificate.pdf" target="_blank">C</a></li>
+      <li><a href="/certificates/cpp-certificate.pdf" target="_blank">C++</a></li>
+      <li><a href="/certificates/java-certificate.pdf" target="_blank">Java</a></li>
     </ul>
+    <p><b>Web Dev and CI/CD</b></p>
+    <ul class="bullet-points">
+      <li><a href="/certificates/fastapi-certificate.pdf" target="_blank">fastAPI</a></li>
+      <li><a href="/certificates/docker-and-k8s-certificate.pdf" target="_blank">Docker and Kubernetes (K8s)</a></li>
+      <li><a href="/certificates/github-actions-certificate.pdf" target="_blank">GitHub Actions and Amazon Web Service (AWS)</a></li>
+      <li><a href="/certificates/jenkins-certificate.pdf" target="_blank">Jenkins and Amazon Web Service (AWS)</a></li>
+      <li><a href="/certificates/web-scraping-with-crapy-certificate.pdf" target="_blank">Web Scraping with Scrapy</a></li>
+      <li><a href="/certificates/postgresql-in-python-certificate.pdf" target="_blank">PostgreSQL in Python</a></li>
+      <li><a href="/certificates/postgresql-certificate.pdf" target="_blank">PostgreSQL</a></li>
+    </ul>
+    <p><b>Computer Science</b></p>
+    <ul class="bullet-points">
+      <li><a href="/certificates/pytorch-certificate.pdf" target="_blank">PyTorch</a></li>
+      <li><a href="/certificates/beginner-game-dev-certificate.pdf" target="_blank">C# Unity Game Developer 3D</a></li>
+      <li><a href="/certificates/intermediate-game-dev-certificate.pdf" target="_blank">Intermediate Unity and C#</a></li>
+      <li><a href="/certificates/multiplayer-game-dev-certificate.pdf" target="_blank">Unity Multiplayer, C# and Networking</a></li>
+      <li><a href="/certificates/ros-certificate.pdf" target="_blank">Robot Operating System (ROS) </a></li>
+      <li><a href="/certificates/ethical-hacking-certificate.pdf" target="_blank">Ethical Hacking</a></li>
+    </ul>
+    <p><b>Non-Technical</b></p>
+    <ul class="bullet-points">
+      <li><a href="/certificates/learning-to-learn-certificate.pdf" target="_blank">Learning to learn</a></li>
+      <li><a href="/certificates/investing-in-stock-certificate.pdf" target="_blank">Investing in Stocks</a></li>
+    </ul>
+    <hr>
 
     <h3>Other</h3>
-    <ul class="other-bullet-points">
-      <li>Oxford english test</li>
+    <ul class="bullet-points">
+      <li><a href="/certificates/oxford-english-test.pdf" target="_blank">Oxford English Test</a></li>
     </ul>
   </div>
 </div>
@@ -34,7 +69,7 @@
 		text-align: right;
 	}
 
-	.certificates-container {
+	.text-content {
 		text-align: left;
 		margin-left: auto;
 		margin-right: 0;
@@ -42,5 +77,4 @@
 		min-width: 300px;
 		max-width: 75ch;
 	}
-
 </style>
