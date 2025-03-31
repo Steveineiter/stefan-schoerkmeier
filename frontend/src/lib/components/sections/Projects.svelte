7429ed355dd7553f786f2c3a67a77de4e7e31433
Stefan Schörkmeier <s.schoerkmeier@gmx.at>
2025-03-31 20:04:25 +0200

    #10 Created rough sketch/architecture for projects

--- frontend/src/lib/components/sections/Projects.svelte
+++ frontend/src/lib/components/sections/Projects.svelte
@@ -2,9 +2,48 @@
   let { title } = $props();
 </script>
 
-<div class="about-container">
+<div class="projects-container">
   <div class="text-content">
     <h2>{title}</h2>
+
+
+    <h3>CERN</h3>
     <p>CERN (da ah empfehlungsschreiben und empoyemnt attensation cern rein hauen), Data Team, Itell solution, personal projects etc</p>
+
+    <h3>Itell Solution</h3>
+    <p>Da halt sagen dass i beim ersten viel non technical skills glernt hab und eben mit kunden reden und die anderen
+    insane wichtig war weil i dann eg naming von der praxis gelernt hab und des immer im studium angewendet hab und halt
+    so viele best practices wie geht dabei eingsetzt hat was alles viel cooler gmacht hat und mir immer was bracht hat</p>
+
+    <h3>Personal Projects</h3>
+    <p>weil i hak gangen bin hatte i null ahnung deswegen hab i soviel gemacht wie geht, es war richtig cool wie i mei erses usefulles programm
+    usen keinan hab (auto close programs). Dann hab i halt die vielen branches fa computer science probiert und eg games gebaut
+    oder raspberry pi als minecraft server aufgesetzt etc (bild fa raspberry pi und fa game dev)</p>
+
+    <h3>TU Graz Data Team</h3>
+    <p>generell driwa schrieben dass cool war ma immer interessante sachen gehoert hat und halt durch copetitiv sachen immer
+    versucht hat cutting edge sachen zu machen. Driwa schreiben dass wia silber griagt haben bei kaggle und mit Thomas und Raffael
+    1. platz bei xeek, des bissl beschrieben und halt a sagen das Thomas is heavy lifting gmacht hat. </p>
+
+    <h3>This Project</h3>
+    <p>I always start with learning (weil is halt wichtig fia quality, scalability etc. - und i bin drauf kommen jedes mal wenn
+      i wo heang liegts drann weil i zu wenig grundlagen hab, deswegen sind starke grundlagen einfach
+      immer is beste - wir wollen ja a super zukunftsreiche software bauen und die braucht halt a stabiles gscheides fundament
+      ... then design ... then learning again for best practices / strucutre and then iterate (developing, desiging, learning)</p>
+    <p>TODO hier wsl auch gut a paar bilder usw rein vom design schaut mby cool aus. E </p>
+
   </div>
 </div>
+
+<style>
+	h2 {
+		text-align: left;
+	}
+
+	.text-content {
+		text-align: left;
+		flex: 1;
+		min-width: 300px;
+		max-width: 75ch;
+	}
+</style>
