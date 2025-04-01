f352e87909a6325810af103b263abc9418f1aaed
Stefan Schörkmeier <s.schoerkmeier@gmx.at>
2025-04-01 14:49:36 +0200

    #10 checked knowledge section with LLM for better writing quality.

--- frontend/src/lib/components/sections/Knowledge.svelte
+++ frontend/src/lib/components/sections/Knowledge.svelte
@@ -6,74 +6,78 @@
   <div class="text-content">
     <h2>{title}</h2>
     <h3>Health & Lifestyle</h3>
-    <p>Health is the most important thing we can have. I think most people would agree. Unfortunately there is so much
-    misinformation out there and many people think diet, for example, is really confusing - me included (I tried many
-      sorts of diet - keto, carnivore, mediterranean, vegan, ...) . This is interesting,
-    because we already know the rough steps to take since the 1980's. Obviously, if you have allergies or an illness these
-    findings might not apply to you - but for 90% or so of the population they do. According to
-      <a href="https://www.youtube.com/watch?v=h2q5qC7NLMY" target="_blank">Dr. Gardner</a> those 7 things are:
+    <p>
+      Health is the most important thing we can have - most people would agree. Unfortunately, there’s so much
+      misinformation out there, and many people find topics like diet confusing (myself included). I’ve tried many diets - keto, carnivore, Mediterranean, vegan, and more.
+      What’s interesting is that we’ve known the rough steps to a healthy diet since the 1980s. Of course, if you have allergies or specific illnesses, these findings might not apply to you.
+      But for 90% of the population, they do. According to
+      <a href="https://www.youtube.com/watch?v=h2q5qC7NLMY" target="_blank">Dr. Gardner</a>, the 7 key principles are:
     </p>
     <ol>
-      <li>Eat a variety of Foods </li>
-      <li>Maintain Ideal Weight </li>
-      <li>Avoid too much Fat, Saturated Fat and Cholesterol </li>
-      <li>Eat foods with Adequate Starch and Fiber </li>
-      <li>Avoid too much Sugar </li>
-      <li>Avoid too much Salt </li>
-      <li>If you drink alcohol, do so in moderation </li>
+      <li>Eat a variety of foods</li>
+      <li>Maintain an ideal weight</li>
+      <li>Avoid too much fat, saturated fat, and cholesterol</li>
+      <li>Eat foods with adequate starch and fiber</li>
+      <li>Avoid too much sugar</li>
+      <li>Avoid too much salt</li>
+      <li>If you drink alcohol, do so in moderation</li>
     </ol>
     <p>
-      From <a href="https://www.youtube.com/watch?v=5s2U4GGBZak" target="_blank">newer research</a> we know that alcohol is even bad
-      in low doses. The cool part about those 7 things is, that we can <b>easily know whether something is healthy</b> or not.
-      Eating a Wiener Schnitzel with fries, a dessert, and a beer each day? Unhealthy since we are breaking each of the findings above
-      (except they are really small portions and you eat great for the rest of the day). We can even put <b>numbers</b> to it.
-      Sugar should be no more than 5% of your daily calories, saturated fats no more than 10% and salt no more than 2.3 grams.
-      If you consume 2500 calories per day (average male) this means around 32g sugar, and around 27 grams of saturated fat.
-      For me, the best diet to archive that is a <b>plant based whole food</b> diet with 1-2 portions of meat/fish per week.
-      Because with this pattern I fulfill the 7 findings by default while still being flexible enough to going out dining with friends.
+      However, <a href="https://www.youtube.com/watch?v=5s2U4GGBZak" target="_blank">newer research</a> suggests that alcohol is harmful even in low doses.
+      The cool part about these 7 principles is that they make it easy to determine whether something is healthy or not.
+      For example, eating a Wiener Schnitzel with fries, dessert, and a beer every day? That’s unhealthy because it breaks almost all the principles above (unless the portions are tiny and the rest of your diet is stellar).
+      We can even quantify this: sugar should be no more than 5% of your daily calories, saturated fats no more than 10%, and salt no more than 2.3 grams.
+      For someone consuming 2,500 calories per day (the average male), this translates to around 32g of sugar and 27g of saturated fat.
     </p>
-    <p> Besides diet, <b>sleep and exercise</b> are the other parts of a healthy life. In a nutshell we want 7-9 hours
-      of high quality sleep, 2-3 hours of weight lifting and 2-3 hours of moderate cardio per week for optimal health.
-      I personally would also add <b>ergonomics</b> (workspace and for sleep) and <b>stress management</b> (meditation, yoga nidra, progressive muscle relaxation)
+    <p>
+      Personally, I find a <b>plant-based whole food diet</b> with 1–2 portions of meat or fish per week works best for me.
+      This approach naturally aligns with the 7 principles while still being flexible enough to enjoy dining out with friends.
+    </p>
+    <p>
+      Beyond diet, <b>sleep and exercise</b> are crucial for a healthy life. In a nutshell, aim for 7–9 hours of high-quality sleep,
+      2–3 hours of weightlifting, and 2–3 hours of moderate cardio per week for optimal health.
+      I’d also add <b>ergonomics</b> (workspace and sleep setup) and <b>stress management</b> (e.g., meditation, yoga nidra, progressive muscle relaxation).
     </p>
     <p><b>Great sources for the above:</b></p>
     <ul>
-      <li><a href="https://www.youtube.com/@NutritionMadeSimple" target="_blank"> Nutrition Made Simple! </a> with Dr. Gil Carvalho PhD (IMHO best source for diet)</li>
-      <li><a href="https://drstanfield.com/en-eu/pages/roadmap?srsltid=AfmBOoqXKSliqQUXfBNRABkXEisT_HbeKm8AVW0AXXYJN_cw8ldFpDsN" target="_blank"> Dr. Brad Stanfield - Roadmap </a></li>
-      <li><a href="https://www.youtube.com/@DrBradStanfield" target="_blank"> Dr. Brad Stanfield - Youtube </a></li>
-      <li><a href="https://www.youtube.com/@JeffNippard" target="_blank">Jeff Nippard </a></li>
-      <li><a href="https://www.youtube.com/@RenaissancePeriodization" target="_blank">Renaissance Periodization  </a> with Michael Israelte, PhD </li>
-      <li><a href="https://www.youtube.com/@DrMiloWolf" target="_blank">Dr. Milo Wolf, PhD </a></li>
+      <li><a href="https://www.youtube.com/@NutritionMadeSimple" target="_blank">Nutrition Made Simple!</a> with Dr. Gil Carvalho, PhD (IMHO the best source for diet)</li>
+      <li><a href="https://drstanfield.com/en-eu/pages/roadmap?srsltid=AfmBOoqXKSliqQUXfBNRABkXEisT_HbeKm8AVW0AXXYJN_cw8ldFpDsN" target="_blank">Dr. Brad Stanfield - Roadmap</a></li>
+      <li><a href="https://www.youtube.com/@DrBradStanfield" target="_blank">Dr. Brad Stanfield - YouTube</a></li>
+      <li><a href="https://www.youtube.com/@JeffNippard" target="_blank">Jeff Nippard</a></li>
+      <li><a href="https://www.youtube.com/@RenaissancePeriodization" target="_blank">Renaissance Periodization</a> with Michael Israetel, PhD</li>
+      <li><a href="https://www.youtube.com/@DrMiloWolf" target="_blank">Dr. Milo Wolf, PhD</a></li>
     </ul>
-    <p>If you wonder, <b>why I only cite science based sources</b> the reason is simple: Time & results. The amazing thing with science
-    is, that it will work for most people. With that I know I will have great progress with a fraction of time investment I would need to make
-    if I follow the advice of a random Influencer (which might even do harm) and try out EVERYTHING whether it works for me or not.</p>
+    <p>
+      If you’re wondering <b>why I only cite science-based sources</b>, the reason is simple: time and results.
+      The amazing thing about science is that it works for most people. This way, I can make great progress with a fraction of the time investment compared to following random influencers (who might even give harmful advice) and trying everything to see what works.
+    </p>
     <hr>
 
-    <h2>Tech channels</h2>
+    <h2>Tech Channels</h2>
     <ul>
-      <li><a href="https://www.youtube.com/@aiexplained-official" target="_blank"> AI Explained </a> - IMHO best source for machine learning/AI news</li>
-      <li><a href="https://www.youtube.com/@Fireship" target="_blank"> Fireship</a> - fun source to stay up to date with developer world</li>
-      <li><a href="https://www.youtube.com/@ArjanCodes" target="_blank"> ArjanCodes</a> - great source for python related topics</li>
-      <li><a href="https://www.youtube.com/@TwoMinutePapers" target="_blank"> Two Minute Papers</a> - source for machine learning/AI news</li>
-      <li><a href="https://www.youtube.com/@fknight" target="_blank"> ForrestKnight</a> - all around / often interesting new topics</li>
-      <li><a href="https://www.youtube.com/@ThePrimeagen" target="_blank"> ThePrimeagen</a> - I basically learned vim motions because of him</li>
+      <li><a href="https://www.youtube.com/@aiexplained-official" target="_blank">AI Explained</a> - IMHO the best source for machine learning/AI news</li>
+      <li><a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a> - A fun way to stay up-to-date with the developer world</li>
+      <li><a href="https://www.youtube.com/@ArjanCodes" target="_blank">ArjanCodes</a> - Great source for Python-related topics</li>
+      <li><a href="https://www.youtube.com/@TwoMinutePapers" target="_blank">Two Minute Papers</a> - Quick updates on machine learning/AI news</li>
+      <li><a href="https://www.youtube.com/@fknight" target="_blank">ForrestKnight</a> - Covers a variety of interesting tech topics</li>
+      <li><a href="https://www.youtube.com/@ThePrimeagen" target="_blank">ThePrimeagen</a> - I basically learned Vim motions because of him</li>
     </ul>
     <hr>
 
-    <h2>Books that taught me a lot</h2>
+    <h2>Books That Taught Me a Lot</h2>
     <ul>
-      <li><b>How to Win Friends & Influence People (Dale Carnegie)</b> - amazing book, I think everybody should read this </li>
-      <li><b>Awaken the Giant Within (Anthony Robbins)</b> - if you have read one selfhelpbook you have read them all, but I
-      liked this one. I got my most important mind set through this book: I can always grow to the kind of person I want to be</li>
-      <li><b>The Lean Startup (Eric Ries)</b> - great read, I think this relevant for each company which works in a dynamic environment </li>
-      <li><b>Zwischen Stahl und Schokolade (Prodanovic; Unterhuber)</b> - interesting stories of austrian businesses and the people behind</li>
-      <li><b>Pattern Recognition and Machine Learning (Christopher Bishop)</b> - I read the whole book in my first term of my Master.
-        I understood around 10% of it so maybe it was a waste of time. But it's an amazing source and I reference it to this date</li>
-      <li><b>Deep Learning (Ian Goodfellow)</b> - same as above</li>
+      <li><b>How to Win Friends & Influence People</b> (Dale Carnegie) - An amazing book; I think everyone should read this</li>
+      <li><b>Awaken the Giant Within</b> (Anthony Robbins) - If you’ve read one self-help book, you’ve read them all, but I liked this one.
+        It gave me my most important mindset: I can always change and grow into the person I want to be.</li>
+      <li><b>The Lean Startup</b> (Eric Ries) - A great read, especially for companies operating in dynamic environments</li>
+      <li><b>Zwischen Stahl und Schokolade</b> (Prodanovic; Unterhuber) - Fascinating stories about Austrian businesses and the people behind them</li>
+      <li><b>Pattern Recognition and Machine Learning</b> (Christopher Bishop) - I read this during my first term of my Master’s.
+        I understood about 10% of it, so maybe it was a waste of time, but it’s an amazing resource that I still reference today.</li>
+      <li><b>Deep Learning</b> (Ian Goodfellow) - Same as above, an excellent resource</li>
     </ul>
   </div>
 </div>
+
 
 
 <style>
